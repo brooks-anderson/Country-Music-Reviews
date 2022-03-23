16b2cccfaadfa9186e15f21953f0a068610d0be1
Brooks Anderson <72112566+brooks-anderson@users.noreply.github.com>
2022-03-23 11:19:01 -0400

    typos

--- cookieFormatter.py
+++ cookieFormatter.py
@@ -8,15 +8,16 @@ Function defition for cookieFormatter()
 def cookieFormatter():
     '''
     cookieFormatter prompts the user to paste web-browsing cookies into the
-    console. It returns the cookies in the form a  dictionary.
+    console. It returns the cookies in the form a dictionary.
     
     The input must be copied from the developer tools window in a web browser.
-    Cookies can be found by navigating to the network tab. Then, selecting the
+    Cookies can be found by navigating to the network tab. Then, the user must
+    find the page session and copy the cookies under Request Headers.
         
 
     Returns
     -------
-    cookie_dict : dictionary
+    cookies : dictionary
         A dictionary where keys are cookie names and values are cookie values
     '''
     raw_dough = input("Please paste cookies:\n\n")
